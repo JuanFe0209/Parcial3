5c70de763123383fc64a950dcdc5532e752ae040
JuanFe0209 <118768558+JuanFe0209@users.noreply.github.com>
2022-11-29 12:43:56 -0500

    Add files via upload

---
+++ atencioncliente.js
@@ -0,0 +1,17 @@
+let cliente = prompt("Por favor presione A si usted es cliente de la cooperativa o B si es cliente externo");
+let servicio = prompt("Presione P si desea entrar a pagos o A si desea entrar a asesoria");
+let resultado
+modulo ["1","2","3"]
+
+
+
+
+
+if(servicio == P);
+{
+    resultado = "Presione C si desea cuotas o A si desea administración";
+} 
+
+
+
+
---
+++ hotel.js
@@ -0,0 +1,21 @@
+let reserva = prompt("Ingrese que tipo de habitacion desea separar: Individual, doble y familiar")
+let personas= prompt("Ingrese el numero de personas que se van a hospedar")
+let fumadores
+let noFumadores
+let resultado
+let individual
+let doble
+let familiar
+
+
+switch (reserva) {
+    case 1:
+        if(individual < 2);
+        {
+            resultado("La reserva se ha realizado exitosamente")
+        }
+           break;
+
+    default:
+        break;
+}
